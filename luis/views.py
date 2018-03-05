46f2fd14d6c4f2369c239d4123d39a7615914479
zender991 <zender991@gmail.com>
2018-03-06 01:00:25 +0200

    add requirements file

--- luis/views.py
+++ luis/views.py
@@ -1,18 +1,28 @@
 from django.shortcuts import render
 from luis.credentials import luis_api_key, luis_url
+from logging.handlers import TimedRotatingFileHandler
+import logging
 import requests
+
+
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+
+handler = TimedRotatingFileHandler('logs/app.log', when="midnight", interval=1)
+handler.setFormatter(formatter)
+
+logger = logging.getLogger("LuisApp")
+handler.suffix = "%Y-%m-%d.log"
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
 
 
 def get_luis_answer(request):
     headers = {
-        # Request headers
         'Ocp-Apim-Subscription-Key': luis_api_key,
     }
 
     params = {
-        # Query parameter
         'q': request.POST.get('luis_query'),
-        # Optional request parameters, set to default values
         'timezoneOffset': '0',
         'verbose': 'false',
         'spellCheck': 'false',
@@ -20,10 +30,14 @@ def get_luis_answer(request):
     }
 
     try:
+        logger.info("Sent query to LUIS - " + request.POST.get('luis_query'))
         r = requests.get(luis_url, headers=headers, params=params)
-        context = {'intent': r.json()}
+        json_response = r.json()
+        logger.info("Response from LUIS - " + str(json_response['topScoringIntent']['intent']))
+        context = {'intent': json_response}
 
     except Exception as e:
         context = {'error': e}
+        logger.error(e)
 
     return render(request, 'luis/luis_answer.html', context)
